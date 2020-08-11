11914c1101d30b3b278d9bb2efbf736cb776b09d
Єгор Грушевий <yegorgru@gmail.com>
2020-08-11 10:35:55 +0300

    TestFramework

---
+++ Red_MIPT_C++/TestFramework/Test.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const std::string& hint = {}) {
+    if (!(t == u)) {
+        std::ostringstream os;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+            os << " hint: " << hint;
+        }
+        throw std::runtime_error(os.str());
+    }
+}
+
+inline void Assert(bool b, const std::string& hint);
+
+class TestRunner {
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const std::string& test_name) {
+        try {
+            func();
+            std::cerr << test_name << " OK" << std::endl;
+        }
+        catch (std::exception& e) {
+            ++fail_count;
+            std::cerr << test_name << " fail: " << e.what() << std::endl;
+        }
+        catch (...) {
+            ++fail_count;
+            std::cerr << "Unknown exception caught" << std::endl;
+        }
+    }
+
+    ~TestRunner();
+
+private:
+    int fail_count = 0;
+};
+
+#define ASSERT_EQUAL(x, y) {            \
+  std::ostringstream os;                \
+  os << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;     \
+  AssertEqual(x, y, os.str());          \
+}
+
+#define ASSERT(x) {                     \
+  std::ostringstream os;                     \
+  os << #x << " is false, "             \
+    << __FILE__ << ":" << __LINE__;     \
+  Assert(x, os.str());                  \
+}
+
+#define RUN_TEST(tr, func) \
+  tr.RunTest(func, #func)
+
+
---
+++ Red_MIPT_C++/TestFramework/TestFramework.cpp
@@ -0,0 +1,22 @@
+﻿#include "Test.h"
+
+TestRunner::~TestRunner() {
+    if (fail_count > 0) {
+        std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
+        exit(1);
+    }
+}
+
+inline void Assert(bool b, const std::string& hint) {
+    AssertEqual(b, true, hint);
+}
+
+void func() {
+    int x = 5;
+    ASSERT_EQUAL(x, 4);
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, func);
+}

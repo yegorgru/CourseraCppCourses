53ab9009a27bfe1e852f1ff7466e0a15d969c9c5
Єгор Грушевий <yegorgru@gmail.com>
2020-09-01 22:47:00 +0300

    week 5 added

---
+++ Red_MIPT_C++/week 5/Josephus problem with move/Josephus_problem.cpp
@@ -0,0 +1,52 @@
+﻿#include "test_runner.h"
+
+#include <cstdint>
+#include <iterator>
+#include <numeric>
+#include <list>
+#include <utility>
+
+using namespace std;
+
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+    list<typename RandomIt::value_type> pool;
+    move(first, last, back_inserter(pool));
+    size_t last_pos = 0;
+    size_t cur_pos = 0;
+    auto it = pool.begin();
+    auto next_it = pool.begin();
+    while (!pool.empty()) {
+        *(first++) = move(*it);
+        next_it = next(it);
+        pool.erase(it);
+        if (pool.empty()) {
+            break;
+        }
+        last_pos = cur_pos;
+        cur_pos = (cur_pos + step_size - 1) % pool.size();
+        if (last_pos < cur_pos && pool.size() - cur_pos > cur_pos - last_pos) {
+            for (size_t i = 0; i < cur_pos - last_pos; i++) {
+                next_it++;
+            }
+        }
+        else if (last_pos < cur_pos) {
+            next_it = pool.end();
+            for (size_t i = 0; i < pool.size() - cur_pos; i++) {
+                --next_it;
+            }
+        }
+        else if (cur_pos > last_pos - cur_pos) {
+            for (size_t i = 0; i < last_pos - cur_pos; i++) {
+                next_it--;
+            }
+        }
+        else {
+            next_it = pool.begin();
+            for (size_t i = 0; i < cur_pos; i++) {
+                next_it++;
+            }
+        }
+        it = next_it;
+    }
+}
---
+++ Red_MIPT_C++/week 5/Merge sorting with move/CourseraRedC++.cpp
@@ -0,0 +1,55 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+using namespace std;
+
+
+//3
+
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+	if (range_end - range_begin < 2) {
+		return;
+	}
+	else {
+		vector<typename RandomIt::value_type> elements(
+			make_move_iterator(range_begin),
+			make_move_iterator(range_end));
+		size_t first = elements.size() / 3;
+		size_t second = elements.size() * 2 / 3;
+		MergeSort(begin(elements), begin(elements) + first);
+		MergeSort(begin(elements) + first, begin(elements) + second);
+		MergeSort(begin(elements) + second, end(elements));
+		vector<typename RandomIt::value_type> tmp;
+		merge(make_move_iterator(begin(elements)),
+			make_move_iterator(begin(elements) + first), 
+			make_move_iterator(begin(elements) + first),
+			make_move_iterator(begin(elements) + second), back_inserter(tmp));
+		merge(make_move_iterator(begin(tmp)), 
+			make_move_iterator(end(tmp)),
+			make_move_iterator(begin(elements) + second),
+			make_move_iterator(end(elements)), range_begin);
+	}
+}
+
+//2
+
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+	if (range_end - range_begin < 2) {
+		return;
+	}
+	else {
+		vector<typename RandomIt::value_type> elements(
+			make_move_iterator(range_begin),
+			make_move_iterator(range_end));
+		size_t middle = elements.size() / 2;
+		MergeSort(begin(elements), begin(elements) + middle);
+		MergeSort(begin(elements) + middle, end(elements));
+		merge(make_move_iterator(begin(elements)), 
+			make_move_iterator(begin(elements) + middle), 
+			make_move_iterator(begin(elements) + middle),
+			make_move_iterator(end(elements)), range_begin);
+	}
+}
